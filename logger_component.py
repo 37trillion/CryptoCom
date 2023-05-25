9d79c184e48a6ecd4b13dcfa0ce5e4943e1d1423
37trillion <96641975+37trillion@users.noreply.github.com>
2023-05-25 00:04:03 +0000

    Update logger_component.py

--- logger_component.py
+++ logger_component.py
@@ -1,7 +1,25 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Configure the root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create a file handler for logging to a file
+file_handler = logging.FileHandler('trading.log')
+file_handler.setLevel(logging.INFO)
+
+# Create a console handler for logging to the console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 def log_info(message):
     logger.info(message)
@@ -9,4 +27,3 @@ def log_info(message):
 def log_error(message):
     logger.error(message)
 
-# You can customize the logger settings based on your needs
